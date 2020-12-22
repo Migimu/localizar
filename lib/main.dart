14bde6556d8aa9cb0816d1a0d462fb245a13d6e6
2dam3 <2dam3@HZ114475.H012108.NET>
2020-12-22 12:13:15 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  CameraPosition _initialPosition =
+      CameraPosition(target: LatLng(26.8206, 30.8025), zoom: 6);
+  Completer<GoogleMapController> _controller = Completer();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationPermission();
+  }
+
+  @override
+  void main(List<String> args) {}
+
+  _getLocationPermission() async {
+    var status = await Permission.location.status;
+    if (status.isUndetermined) {
+      Map<Permission, PermissionStatus> statuses = await [
+        Permission.location,
+      ].request();
+      print(statuses[Permission.location]);
+    }
+    // You can can also directly ask the permission about its status.
+    if (await Permission.location.isRestricted) {
+      //_getLocationPermission();
+      // The OS restricts access, for example because of parental controls.
+    }
+    if (await Permission.location.request().isGranted) {
+      print("@@@@@@@@@@@@@@@@@@@object################");
+      _goToTheUser();
+    }
+    // You can request multiple permissions at once.
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    return Future.delayed(Duration(seconds: 3), () {
+      return Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    });
+  }
+
+  Future<void> _goToTheUser() async {
+    print(await _getCurrentLocation());
+    _currentPosition = await _getCurrentLocation();
+    print(_currentPosition);
+    /*print(_currentPosition.longitude);
+    print(_currentPosition.latitude);*/
+
+    LatLng latLngPosition =
+        LatLng(_currentPosition.latitude, _currentPosition.longitude);
+
+    CameraPosition cameraPosition =
+        new CameraPosition(target: latLngPosition, zoom: 14);
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: _initialPosition,
+          ),
+        ],
+      ),
+    );
+  }
+}
